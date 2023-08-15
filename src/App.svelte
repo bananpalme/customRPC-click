6c069cec735fb3a24fbf96cfa038407840229ca3
bananpalme <nigginz12@gmail.com>
2023-08-16 01:17:32 +0200

    customRPC start

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+	
+</script>
+
+<main>
+	<h1>Hello</h1>
+</main>
+
+<style>
+	main {
+		position: absolute;
+		left: 0;
+		top: 0;
+		text-align: center;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(40, 55, 43, 0.8);
+	}
+	
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
